d4b0b961171d4b1d461a377f9713e8bbabca24b9
Mike <caoxing9@gmail.com>
2024-11-21 21:53:38 +0800

    fix: search relative (#1102)
    * perf: add search hightlight contrast
    
    * perf: update search option display
    
    * fix: search error when table field search cache does not target
    
    * test: add formula_boolean field to search test
    
    * chore: delete useless code
    
    * fix: exchange search mode should reset filter text
    
    * fix: change search filter option do not update row count

--- apps/nextjs-app/src/features/app/blocks/view/search/SearchCommand.tsx
+++ apps/nextjs-app/src/features/app/blocks/view/search/SearchCommand.tsx
@@ -14,7 +14,7 @@ import {
   Toggle,
 } from '@teable/ui-lib';
 import { useTranslation } from 'next-i18next';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 
 interface ISearchCommand {
   value: string;
@@ -56,43 +56,33 @@ export const SearchCommand = (props: ISearchCommand) => {
 
   const enableGlobalSearch = value === 'all_fields';
 
+  const [filterText, setFilterText] = useState('');
+
   return (
     <Command filter={commandFilter}>
-      <div className="flex items-center justify-around gap-1">
-        <Toggle
-          pressed={enableGlobalSearch}
-          onPressedChange={(pressed) => {
-            onChange(pressed ? ['all_fields'] : null);
-          }}
-          size={'sm'}
-          className="flex flex-1 items-center p-0"
-        >
-          <span className="text-sm">{t('actions.globalSearch')}</span>
-        </Toggle>
-
-        <Toggle
-          pressed={!!hideNotMatchRow}
-          onPressedChange={(pressed) => {
-            onHideSwitchChange(pressed);
-          }}
-          size={'sm'}
-          className="flex flex-1 items-center truncate p-0"
-        >
-          <span className="truncate text-sm" title={t('actions.hideNotMatchRow')}>
-            {t('actions.hideNotMatchRow')}
-          </span>
-        </Toggle>
-      </div>
-      {!enableGlobalSearch && (
+      {
         <>
-          <CommandInput placeholder={t('actions.search')} className="h-8 text-xs" />
+          <CommandInput
+            placeholder={t('actions.search')}
+            className="h-8 text-xs"
+            disabled={enableGlobalSearch}
+            value={filterText}
+            onValueChange={(value) => {
+              setFilterText(value);
+            }}
+          />
           <CommandList className="my-2 max-h-64">
             {<CommandEmpty>{t('listEmptyTips')}</CommandEmpty>}
             {fields.map((field) => {
               const { id, name, type, isLookup } = field;
               const { Icon } = fieldStaticGetter(type, isLookup);
               return (
-                <CommandItem className="flex flex-1 truncate p-0" key={id} value={id}>
+                <CommandItem
+                  className="flex flex-1 truncate p-0"
+                  key={id}
+                  value={id}
+                  disabled={enableGlobalSearch}
+                >
                   <TooltipProvider>
                     <Tooltip>
                       <TooltipTrigger asChild>
@@ -104,7 +94,7 @@ export const SearchCommand = (props: ISearchCommand) => {
                             <Switch
                               id={id}
                               className="scale-75"
-                              checked={selectedFields.includes(id)}
+                              checked={selectedFields.includes(id) || enableGlobalSearch}
                               onCheckedChange={(checked) => {
                                 switchChange(id, checked);
                               }}
@@ -132,7 +122,66 @@ export const SearchCommand = (props: ISearchCommand) => {
             })}
           </CommandList>
         </>
-      )}
+      }
+
+      <div className="flex flex-col gap-y-1">
+        <div className="flex items-center justify-around gap-1">
+          <Toggle
+            pressed={enableGlobalSearch}
+            onPressedChange={() => {
+              onChange(['all_fields']);
+              setFilterText('');
+            }}
+            size={'sm'}
+            className="flex flex-1 items-center truncate p-0"
+          >
+            <span className="truncate text-sm" title={t('actions.hideNotMatchRow')}>
+              {t('actions.globalSearch')}
+            </span>
+          </Toggle>
+
+          <Toggle
+            pressed={!enableGlobalSearch}
+            onPressedChange={() => {
+              onChange(null);
+            }}
+            size={'sm'}
+            className="flex flex-1 items-center truncate p-0"
+          >
+            <span className="truncate text-sm" title={t('actions.hideNotMatchRow')}>
+              {t('actions.fieldSearch')}
+            </span>
+          </Toggle>
+        </div>
+
+        <div className="flex items-center justify-around gap-1">
+          <Toggle
+            pressed={!hideNotMatchRow}
+            onPressedChange={() => {
+              onHideSwitchChange(false);
+            }}
+            size={'sm'}
+            className="flex flex-1 items-center truncate p-0"
+          >
+            <span className="truncate text-sm" title={t('actions.hideNotMatchRow')}>
+              {t('actions.showAllRow')}
+            </span>
+          </Toggle>
+
+          <Toggle
+            pressed={!!hideNotMatchRow}
+            onPressedChange={() => {
+              onHideSwitchChange(true);
+            }}
+            size={'sm'}
+            className="flex flex-1 items-center truncate p-0"
+          >
+            <span className="truncate text-sm" title={t('actions.hideNotMatchRow')}>
+              {t('actions.hideNotMatchRow')}
+            </span>
+          </Toggle>
+        </div>
+      </div>
     </Command>
   );
 };
